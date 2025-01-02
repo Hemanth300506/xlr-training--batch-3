48a1d95f4933bb135ce7504e3ed2b10996bf5576
Hemanth300506 <hemanthrao485@gmail.com>
2025-01-02 15:05:52 +0530

    my first commit in gethub

---
+++ LoginApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LoginApp {
+    public static void main(String[] args) {
+        String correctId = "user123";
+        String correctPassword = "password456";
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter ID: ");
+        String id = scanner.nextLine();
+        
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+        
+        if (id.equals(correctId) && password.equals(correctPassword)) {
+            System.out.println("Happy New Year!");
+        } else {
+            System.out.println("Invalid ID or password.");
+        }
+    
+        scanner.close();
+    }
+}
