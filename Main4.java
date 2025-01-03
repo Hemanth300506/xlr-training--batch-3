b7666e86887f1db2ad7a8bb6cbc562ded3792e4d
Hemanth300506 <hemanthrao485@gmail.com>
2025-01-03 10:43:45 +0530

    using while loop condition

---
+++ Main4.java
@@ -0,0 +1,12 @@
+public class Main4 {
+
+    public static void main(String[] args) {
+        String name = "Hemanth";
+        String password = "Hemanth@30";
+        while((name == "Hemanth") && (password == "Hemanth@30")) {
+            System.out.println("hello hemanth happy new year");
+            break;
+        }
+    }
+    
+}
