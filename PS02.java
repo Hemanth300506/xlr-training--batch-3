19081805e9b4909b73a46a735338962d39d9281b
Hemanth300506 <hemanthrao485@gmail.com>
2025-01-08 11:05:01 +0530

    Next multiple of 100

--- PS02.java
+++ PS02.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+class NextMultipleOf100 {
+    
+    public static int getNextMultipleOf100(int num) {
+         if (num <= 0)
+             return -1;
+         return ((num / 100) + 1) * 100;
+    }
+}
 
 public class PS02 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        if (number % 100 == 0) {
-            System.out.println(number + " is a multiple of 100.");
-        } else {
-            System.out.println(number + " is not a multiple of 100.");
-        }
-        scanner.close();
+        NextMultipleOf100 obj = new NextMultipleOf100();
+        int num = 223;
+        System.out.println(obj.getNextMultipleOf100(num));
     }
+    
 }
