cc5d18389b2bee5cc39e8a84d05202fcbdf80885
Hemanth300506 <hemanthrao485@gmail.com>
2025-01-08 10:54:50 +0530

    Multiple of 100

---
+++ PS02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PS02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (number % 100 == 0) {
+            System.out.println(number + " is a multiple of 100.");
+        } else {
+            System.out.println(number + " is not a multiple of 100.");
+        }
+        scanner.close();
+    }
+}
