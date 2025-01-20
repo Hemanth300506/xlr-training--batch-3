eb57a293f779ae31c0caea34985a43fef9c8b9da
Hemanth300506 <hemanthrao485@gmail.com>
2025-01-20 12:49:24 +0530

    adding 10 names in arraylist

---
+++ Demo028.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Demo028{
+    ArrayList<String> list = new ArrayList<String>();
+    void Add(String name){
+        list.add(name);
+    }
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<10; i++){
+            System.out.print("Enter the "+(i+1)+" person name: ");
+            String name = sc.next();
+            Add(name);
+        }
+    }
+    void display(){
+        for(String name: list){
+            System.out.println(name);
+        }
+    }
+    public static void main(String[] args){
+        Demo028 obj = new Demo028();
+        obj.input();
+        System.out.println("\nNames are: ");
+        obj.display();
+    }
+}
