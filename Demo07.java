7f49c466fa8acdf96017e3cb6a37fce0f8091eb8
Hemanth300506 <hemanthrao485@gmail.com>
2025-02-04 12:42:11 +0530

    user input

---
+++ Demo07.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Demo07{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String username,pass;
+        do{
+            System.out.println("Welcome to mallareddy university, please login");
+            System.out.println("Enter name: ");
+            username = sc.next();
+            System.out.println("Enter Passwrd: ");
+            pass = sc.next();
+            }   
+        While(!username.equals(anObject:"Hemanth")) || (!pass.equals(anObject:"Hemanth_30"))
+        }
+    }
